1a59f5b52fd4ea1e63d83748ac417348f2119fae
steven <steven.lam1@outlook.com>
2020-06-02 18:31:41 +1000

    first site

--- script.py
+++ script.py
@@ -7,36 +7,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 from datetime import datetime
+import time
 
-# Installed chromedriver in the path chrome/chromedriver
-browser = webdriver.Chrome(executable_path="chrome/chromedriver")
-browser.get("https://www.worldometers.info/coronavirus")
+import json
 
-# Wait 10 seconds for page to load
-timeout = 10
+MSG_TXT = dict()
 
-# Wait for the daily deaths chart to load, since it is the last thing to load in this website
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.tabbable-panel-deaths")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    browser.quit()
+def get_total():
+    # Installed chromedriver in the path chrome/chromedriver
+    browser = webdriver.Chrome(executable_path="chrome/chromedriver")
+    browser.get("https://www.worldometers.info/coronavirus")
 
-new = browser.find_element_by_xpath("//tr[@class= 'total_row']")
-row = new.find_element_by_xpath("./..")
-data = row.text.split(" ")
+    # Wait 10 seconds for page to load
+    timeout = 10
 
-world_total = []
+    # Wait for the daily deaths chart to load, since it is the last thing to load in this website
+    try:
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.tabbable-panel-deaths")))
+    except TimeoutException:
+        print("Timed out waiting for page to load")
+        browser.quit()
 
-total_cases = data[1]
-new_cases_today = data[2]
-total_deaths = data[3]
-new_deaths_today = data[4]
-total_recovered = data[5]
-active_cases = data[6]
+    new = browser.find_element_by_xpath("//tr[@class='total_row']")
+    row = new.find_element_by_xpath("./..")
+    
+    print(row.text.split(" "))
 
-now = datetime.now()
-time = now.strftime("%Y-%m-%d %H:%M:%S")
+    data = row.text.split(" ")
 
-print("Current Time =", time)
+    #print(data)
 
+
+    MSG_TXT['total_cases'] = data[1]
+    MSG_TXT['new_cases_today'] = data[2]
+    MSG_TXT['total_deaths'] = data[3]
+    MSG_TXT['new_deaths_today'] = data[4]
+    MSG_TXT['total_recovered'] = data[5]
+    MSG_TXT['active_cases'] = data[6]
+
+    now = datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    news = browser.find_elements_by_xpath("//li[@class= 'news_li']")
+    news_data = [x.text for x in news[:5]]
+    clean_news = [x.replace('[source]','') for x in news_data]
+
+    print(clean_news)
+
+    print("Current Time =", time)
+
+if __name__ == '__main__':
+    print ( "Getting data\n" )
+    get_total()
+    final_msg = json.dumps(MSG_TXT)
+    print(final_msg)
